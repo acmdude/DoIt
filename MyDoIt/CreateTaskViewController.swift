444b2a874bbbc0a2e62b6bf5e040ef2e1706e1f8
Angelo Micheletti <cps@bendbroadband.com>
2016-09-27 07:22:57 -0700

    Second controller designed.

---
+++ MyDoIt/CreateTaskViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateTaskViewController.swift
+//  MyDoIt
+//
+//  Created by Angelo Micheletti on 9/24/16.
+//  Copyright © 2016 Angelo Micheletti. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+    
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+        
+        //Create new task when Add buttom tapped.  Then add task to array in other viewController.
+        
+        
+        
+    }
+    
+    
+    
+   
+
+}
---
+++ MyDoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  MyDoIt
+//
+//  Created by Angelo Micheletti on 9/23/16.
+//  Copyright © 2016 Angelo Micheletti. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
+
+
